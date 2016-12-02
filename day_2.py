31f65b2a151779f1bd1322f7bbb23b0b203e4df4
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2016-12-02 20:54:13 +0100

    done with second task for day 2

--- day_2.py
+++ day_2.py
@@ -22,13 +22,60 @@ class KeyPad(object):
     def getCurrentKey(self):
         return self.keys[self.x + self.y * self.size]
 
+class WtfKeyPad(object):
+
+    def __init__(self):
+        self.x = 0
+        self.y = 2
+        self.denied_indexes = [0, 1, 3, 4, 5, 9, 15, 19, 20, 21, 23, 24]
+        self.size = 5
+        self.keys = ['0', '0', '1', '0', '0',
+                     '0', '2', '3', '4', '0',
+                     '5', '6', '7', '8', '9',
+                     '0', 'A', 'B', 'C', '0',
+                     '0', '0', 'D', '0', '0'
+        ]
+
+    def move(self, dir):
+        if dir == 'U':
+            self.y -= 1
+            if self.isDeniedPosition(self.x, self.y):
+                self.y += 1
+        if dir == 'D':
+            self.y += 1
+            if self.isDeniedPosition(self.x, self.y):
+                self.y -= 1
+        if dir == 'L':
+            self.x -= 1
+            if self.isDeniedPosition(self.x, self.y):
+                self.x += 1
+        if dir == 'R':
+            self.x += 1
+            if self.isDeniedPosition(self.x, self.y):
+                self.x -= 1
+
+        self.x = max(0, min(self.x, self.size-1))
+        self.y = max(0, min(self.y, self.size-1))
+
+    def isDeniedPosition(self, x, y):
+        return (x + y * self.size) in self.denied_indexes
+
+    def getCurrentKey(self):
+        return self.keys[self.x + self.y * self.size]
+
 
 input = file('day_2.input', 'r').readlines()
 keypad = KeyPad()
-password = []
+wtfKeypad = WtfKeyPad()
+password1 = []
+password2 = []
 
 for line in input:
     for letter in line:
         keypad.move(letter)
-    password.append(keypad.getCurrentKey())
-print password
+        wtfKeypad.move(letter)
+    password1.append(keypad.getCurrentKey())
+    password2.append(wtfKeypad.getCurrentKey())
+
+print password1
+print password2
