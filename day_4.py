fb0634a0cb97fe4ff63432622bfcfb882e372f8f
Klemen Brajkovic <klemen.brajkovic@outfit7.com>
2016-12-04 14:51:25 +0100

    day 4 task 2 done

--- day_4.py
+++ day_4.py
@@ -1,8 +1,5 @@
 import re, operator
 
-input = file('day_4.input', 'r').readlines()
-sum = 0
-
 def isLetterInSublist(list, letter):
     for i in range(len(list)):
         if list[i][0] == letter:
@@ -16,27 +13,58 @@ def incrementListLetter(list, letter):
     else:
         list.append([letter, 0])
 
-for line in input:
-    parse = re.findall(r'([a-z]*?)-|([0-9]{1,4})\[([a-z]*?)\]', line)
+def decrypt(list, id):
+    new_list = []
+    for r in list:
+        word = ''
+        for l in r:
+            word += chr((((ord(l) - 97) + (id % 26)) % 26) + 97)
+        new_list.append(word)
+    return new_list
 
-    room_letter_order = {}
-    room_letters = []
-    most_common_letters = ''
-    id = parse[-1][1]
-    hash = parse[-1][2]
+def task_1(input):
+    sum = 0
 
-    for p in parse[:-1]:
-        room_name = p[0]
-        for r in room_name:
-            incrementListLetter(room_letters, r)
+    for line in input:
+        parse = re.findall(r'([a-z]*?)-|([0-9]{1,4})\[([a-z]*?)\]', line)
 
-    t = sorted(room_letters, key=operator.itemgetter(0), reverse=False)
-    t = sorted(t, key=operator.itemgetter(1), reverse=True)
+        room_letters = []
+        most_common_letters = ''
+        id = parse[-1][1]
+        hash = parse[-1][2]
+        room_name = [i[0] for i in parse[:-1]]
 
-    for i in t[:5]:
-        most_common_letters += i[0]
+        for p in room_name:
+            for r in p:
+                incrementListLetter(room_letters, r)
 
-    if most_common_letters == hash:
-        sum += int(id)
+        t = sorted(room_letters, key=operator.itemgetter(0), reverse=False)
+        t = sorted(t, key=operator.itemgetter(1), reverse=True)
 
-print sum
+        for i in t[:5]:
+            most_common_letters += i[0]
+
+        if most_common_letters == hash:
+            sum += int(id)
+
+    return sum
+
+def task_2(input):
+
+    for line in input:
+        parse = re.findall(r'([a-z]*?)-|([0-9]{1,4})\[([a-z]*?)\]', line)
+
+        id = parse[-1][1]
+        room_name = [i[0] for i in parse[:-1]]
+
+        room_name_d = decrypt(room_name, int(id))
+
+        for name in room_name_d:
+            if 'north' in name:
+                return [room_name_d, id]
+
+    return None
+
+input = file('day_4.input', 'r').readlines()
+print task_1(input)
+print task_2(input)
